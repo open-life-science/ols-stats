594a01435984ecd73d04e5a19b2cdc7ca7c5e2e8
bebatut <bebatut@gmail.com>
2023-02-14 16:35:14 +0100

    Add continent information from country and also country aplha3

--- src/extract_data_from_website.py
+++ src/extract_data_from_website.py
@@ -3,14 +3,263 @@
 import argparse
 import base64
 import copy
-import datetime
 import pandas as pd
+import pycountry
 import yaml
 
 from github import Github
 from pathlib import Path
 
-actual_cohort = 7
+ACTUAL_COHORT = 7
+
+# copied from https://github.com/jefftune/pycountry-convert/blob/master/pycountry_convert/country_alpha2_to_continent.py
+COUNTRY_ALPHA2_TO_CONTINENT = {
+    'AB': 'Asia',
+    'AD': 'Europe',
+    'AE': 'Asia',
+    'AF': 'Asia',
+    'AG': 'North America',
+    'AI': 'North America',
+    'AL': 'Europe',
+    'AM': 'Asia',
+    'AO': 'Africa',
+    'AR': 'South America',
+    'AS': 'Oceania',
+    'AT': 'Europe',
+    'AU': 'Oceania',
+    'AW': 'North America',
+    'AX': 'Europe',
+    'AZ': 'Asia',
+    'BA': 'Europe',
+    'BB': 'North America',
+    'BD': 'Asia',
+    'BE': 'Europe',
+    'BF': 'Africa',
+    'BG': 'Europe',
+    'BH': 'Asia',
+    'BI': 'Africa',
+    'BJ': 'Africa',
+    'BL': 'North America',
+    'BM': 'North America',
+    'BN': 'Asia',
+    'BO': 'South America',
+    'BQ': 'North America',
+    'BR': 'South America',
+    'BS': 'North America',
+    'BT': 'Asia',
+    'BV': 'Antarctica',
+    'BW': 'Africa',
+    'BY': 'Europe',
+    'BZ': 'North America',
+    'CA': 'North America',
+    'CC': 'Asia',
+    'CD': 'Africa',
+    'CF': 'Africa',
+    'CG': 'Africa',
+    'CH': 'Europe',
+    'CI': 'Africa',
+    'CK': 'Oceania',
+    'CL': 'South America',
+    'CM': 'Africa',
+    'CN': 'Asia',
+    'CO': 'South America',
+    'CR': 'North America',
+    'CU': 'North America',
+    'CV': 'Africa',
+    'CW': 'North America',
+    'CX': 'Asia',
+    'CY': 'Asia',
+    'CZ': 'Europe',
+    'DE': 'Europe',
+    'DJ': 'Africa',
+    'DK': 'Europe',
+    'DM': 'North America',
+    'DO': 'North America',
+    'DZ': 'Africa',
+    'EC': 'South America',
+    'EE': 'Europe',
+    'EG': 'Africa',
+    'ER': 'Africa',
+    'ES': 'Europe',
+    'ET': 'Africa',
+    'FI': 'Europe',
+    'FJ': 'Oceania',
+    'FK': 'South America',
+    'FM': 'Oceania',
+    'FO': 'Europe',
+    'FR': 'Europe',
+    'GA': 'Africa',
+    'GB': 'Europe',
+    'GD': 'North America',
+    'GE': 'Asia',
+    'GF': 'South America',
+    'GG': 'Europe',
+    'GH': 'Africa',
+    'GI': 'Europe',
+    'GL': 'North America',
+    'GM': 'Africa',
+    'GN': 'Africa',
+    'GP': 'North America',
+    'GQ': 'Africa',
+    'GR': 'Europe',
+    'GS': 'South America',
+    'GT': 'North America',
+    'GU': 'Oceania',
+    'GW': 'Africa',
+    'GY': 'South America',
+    'HK': 'Asia',
+    'HM': 'Antarctica',
+    'HN': 'North America',
+    'HR': 'Europe',
+    'HT': 'North America',
+    'HU': 'Europe',
+    'ID': 'Asia',
+    'IE': 'Europe',
+    'IL': 'Asia',
+    'IM': 'Europe',
+    'IN': 'Asia',
+    'IO': 'Asia',
+    'IQ': 'Asia',
+    'IR': 'Asia',
+    'IS': 'Europe',
+    'IT': 'Europe',
+    'JE': 'Europe',
+    'JM': 'North America',
+    'JO': 'Asia',
+    'JP': 'Asia',
+    'KE': 'Africa',
+    'KG': 'Asia',
+    'KH': 'Asia',
+    'KI': 'Oceania',
+    'KM': 'Africa',
+    'KN': 'North America',
+    'KP': 'Asia',
+    'KR': 'Asia',
+    'KW': 'Asia',
+    'KY': 'North America',
+    'KZ': 'Asia',
+    'LA': 'Asia',
+    'LB': 'Asia',
+    'LC': 'North America',
+    'LI': 'Europe',
+    'LK': 'Asia',
+    'LR': 'Africa',
+    'LS': 'Africa',
+    'LT': 'Europe',
+    'LU': 'Europe',
+    'LV': 'Europe',
+    'LY': 'Africa',
+    'MA': 'Africa',
+    'MC': 'Europe',
+    'MD': 'Europe',
+    'ME': 'Europe',
+    'MF': 'North America',
+    'MG': 'Africa',
+    'MH': 'Oceania',
+    'MK': 'Europe',
+    'ML': 'Africa',
+    'MM': 'Asia',
+    'MN': 'Asia',
+    'MO': 'Asia',
+    'MP': 'Oceania',
+    'MQ': 'North America',
+    'MR': 'Africa',
+    'MS': 'North America',
+    'MT': 'Europe',
+    'MU': 'Africa',
+    'MV': 'Asia',
+    'MW': 'Africa',
+    'MX': 'North America',
+    'MY': 'Asia',
+    'MZ': 'Africa',
+    'NA': 'Africa',
+    'NC': 'Oceania',
+    'NE': 'Africa',
+    'NF': 'Oceania',
+    'NG': 'Africa',
+    'NI': 'North America',
+    'NL': 'Europe',
+    'NO': 'Europe',
+    'NP': 'Asia',
+    'NR': 'Oceania',
+    'NU': 'Oceania',
+    'NZ': 'Oceania',
+    'OM': 'Asia',
+    'OS': 'Asia',
+    'PA': 'North America',
+    'PE': 'South America',
+    'PF': 'Oceania',
+    'PG': 'Oceania',
+    'PH': 'Asia',
+    'PK': 'Asia',
+    'PL': 'Europe',
+    'PM': 'North America',
+    'PR': 'North America',
+    'PS': 'Asia',
+    'PT': 'Europe',
+    'PW': 'Oceania',
+    'PY': 'South America',
+    'QA': 'Asia',
+    'RE': 'Africa',
+    'RO': 'Europe',
+    'RS': 'Europe',
+    'RU': 'Europe',
+    'RW': 'Africa',
+    'SA': 'Asia',
+    'SB': 'Oceania',
+    'SC': 'Africa',
+    'SD': 'Africa',
+    'SE': 'Europe',
+    'SG': 'Asia',
+    'SH': 'Africa',
+    'SI': 'Europe',
+    'SJ': 'Europe',
+    'SK': 'Europe',
+    'SL': 'Africa',
+    'SM': 'Europe',
+    'SN': 'Africa',
+    'SO': 'Africa',
+    'SR': 'South America',
+    'SS': 'Africa',
+    'ST': 'Africa',
+    'SV': 'North America',
+    'SY': 'Asia',
+    'SZ': 'Africa',
+    'TC': 'North America',
+    'TD': 'Africa',
+    'TG': 'Africa',
+    'TH': 'Asia',
+    'TJ': 'Asia',
+    'TK': 'Oceania',
+    'TM': 'Asia',
+    'TN': 'Africa',
+    'TO': 'Oceania',
+    'TP': 'Asia',
+    'TR': 'Asia',
+    'TT': 'North America',
+    'TV': 'Oceania',
+    'TW': 'Asia',
+    'TZ': 'Africa',
+    'UA': 'Europe',
+    'UG': 'Africa',
+    'US': 'North America',
+    'UY': 'South America',
+    'UZ': 'Asia',
+    'VC': 'North America',
+    'VE': 'South America',
+    'VG': 'North America',
+    'VI': 'North America',
+    'VN': 'Asia',
+    'VU': 'Oceania',
+    'WF': 'Oceania',
+    'WS': 'Oceania',
+    'XK': 'Europe',
+    'YE': 'Asia',
+    'YT': 'Africa',
+    'ZA': 'Africa',
+    'ZM': 'Africa',
+    'ZW': 'Africa',
+}
 
 def read_yaml_file(fp, ref):
     '''
@@ -65,11 +314,14 @@ def get_people_names(p_list, p_dict):
 
 def get_people():
     '''
-    Get people information
+    Get and format people information
     '''
+    print("Get and format people information")
+    # get people information
     people = read_yaml_file("_data/people.yaml", "main")
-    # remove some keys and add space for cohorts
+    # format information
     for key, value in people.items():
+        # remove some keys
         value.pop('affiliation', None)
         value.pop('bio', None)
         value.pop('orcid', None)
@@ -78,8 +330,25 @@ def get_people():
         value.pop('github', None)
         value.pop('title', None)
         value.pop('expertise', None)
-        for i in range(1, actual_cohort+1):
+        # get country alpha_3 and continent
+        value['country-alpha_3'] = None
+        value['continent'] = None
+        if 'country' in value:
+            country = pycountry.countries.get(name=value['country'])
+            if country is None:
+                country = pycountry.countries.get(common_name=value['country'])
+                if country is None:
+                    print(f"{value['country']} not found")
+            else:
+                value[f'country-alpha_3'] = country.alpha_3
+                if country.alpha_2 not in COUNTRY_ALPHA2_TO_CONTINENT:
+                    print(f"No continent found for {value['country']} / {country.alpha_2}")
+                else:
+                    value['continent'] = COUNTRY_ALPHA2_TO_CONTINENT[country.alpha_2]
+        # add space for cohorts
+        for i in range(1, ACTUAL_COHORT+1):
             value[f'ols-{i}'] = []
+        
     return people
 
 
@@ -87,8 +356,9 @@ def extract_cohort_information(people):
     '''
     Get cohort and project informations
     '''
+    print("Get cohort and project informations")
     projects = []
-    for i in range(1, actual_cohort+1):
+    for i in range(1, ACTUAL_COHORT+1):
         print(f"OLS {i}")
         # extract experts, facilitators, organizers from metadata
         metadata = read_yaml_file(f"_data/ols-{i}-metadata.yaml" , "main")
@@ -141,7 +411,7 @@ if __name__ == '__main__':
 
     # export people information to CSV file
     people_df = pd.DataFrame.from_dict(people, orient='index')
-    for i in range(1, actual_cohort+1):
+    for i in range(1, ACTUAL_COHORT+1):
         people_df[f'ols-{i}'] = people_df[f'ols-{i}'].apply(lambda x: ', '.join([str(i) for i in x]))
     people_fp = Path(args.out) / Path('people.csv')
     people_df.to_csv(people_fp)
